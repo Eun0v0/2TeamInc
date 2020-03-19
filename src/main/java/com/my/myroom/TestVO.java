f986791bf55bebad19776139cf2af19dfe4bca6c
eun0v0 <yukihana0726@naver.com>
2020-03-19 17:48:54 +0900

    chaeeun commit

---
+++ src/main/java/com/my/myroom/TestVO.java
@@ -0,0 +1,11 @@
+package com.my.myroom;
+
+import lombok.Data;
+
+@Data
+public class TestVO {
+	int a;
+	int b;
+	
+	
+}
--- src/main/java/com/my/myroom/map/MapController.java
+++ src/main/java/com/my/myroom/map/MapController.java
@@ -1,9 +1,8 @@
-package com.my.myroom;
+package com.my.myroom.map;
 
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
-
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,23 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import lombok.extern.slf4j.Slf4j;
-/**
- * Handles requests for the application home page.
- */
+
 @Slf4j
 @Controller
-public class HomeController {
-	
-	//private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+public class MapController {
+	@RequestMapping(value = "/mainMap", method = RequestMethod.GET)
+	public String mainMap(Locale locale, Model model) {
 		//log.info("Welcome home! The client locale is {}.", locale);
 		
-		log.info("Hello");
+		log.info("Map");
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
@@ -35,7 +26,6 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "home";
+		return "mainMap";
 	}
-	
 }
